0dcbf49fe338259ee42b395f3c6de4048e0edcb7
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-07-28 22:46:40 -0300

    Frontend

---
+++ Backend/src/main/java/com/testlify/testlify/TestlifyApplication.java
@@ -0,0 +1,13 @@
+package com.testlify.testlify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestlifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestlifyApplication.class, args);
+	}
+
+}
