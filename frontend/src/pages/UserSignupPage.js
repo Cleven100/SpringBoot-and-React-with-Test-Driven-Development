cd7ca35df595c70d124d12cc2e623c944c03d39f
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-08-02 20:04:47 -0300

    User Signup Page

---
+++ frontend/src/pages/UserSignupPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+ 
+// one of them is here at the class definition
+export class UserSignupPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Sign Up</h1>
+      </div>
+    );
+  }
+}
+ 
+// and the other one is here for default export
+export default UserSignupPage;
