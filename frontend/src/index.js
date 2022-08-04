3e1a565959c1955da9913ce183a7324f25336065
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-08-04 15:11:55 -0300

    End

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { UserSignupPage } from './pages/UserSignupPage';
+import { HashRouter } from 'react-router-dom';
+import * as serviceWorker from './serviceWorker';
+import App from './containers/App';
+import { Provider } from 'react-redux';
+import configureStore from './redux/configureStore';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <UserSignupPage />
-  </React.StrictMode>
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
